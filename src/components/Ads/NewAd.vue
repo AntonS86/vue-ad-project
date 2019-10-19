c223fa6a05582cb9af0e76ae3206999230bd8854
antonS <so4usertrue@gmail.com>
2019-10-19 16:13:50 +0300

    Update page Ordes and NewAd

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,67 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>New ad</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3">Create new ad</h1>
+                <v-form ref="form"
+                        class="mb-3"
+                        v-model="valid"
+                        lazy-validation>
+                    <v-text-field
+                            label="Ad title"
+                            name="title"
+                            type="text"
+                            required
+                            :rules="[v => !!v || 'Title is required']"
+                            v-model="title"
+                    ></v-text-field>
+                    <v-textarea
+                            v-model="description"
+                            name="description"
+                            :rules="[v => !!v || 'Description is required']"
+                    >
+                        <template v-slot:label>
+                            <div>
+                                Label ad description
+                            </div>
+                        </template>
+                    </v-textarea>
+                </v-form>
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-btn
+                                class="warning"
+                        >
+                            Upload
+                            <v-icon right dark>mdi-cloud-upload</v-icon>
+                        </v-btn>
+                        <v-layout row>
+                            <v-flex xs12>
+                                <img src="" height="100">
+                            </v-flex>
+                        </v-layout>
+                        <v-layout row>
+                            <v-flex xs12>
+                                <v-switch
+                                        color="primary"
+                                        v-model="promo"
+                                        label="Ad to promo"
+                                ></v-switch>
+                            </v-flex>
+                        </v-layout>
+                        <v-layout row>
+                            <v-flex xs12>
+                                <v-spacer></v-spacer>
+                               <v-btn
+                                       class="success"
+                                       @click="createAd"
+                                       :disabled="!valid"
+                               >Create Ad</v-btn>
+                            </v-flex>
+                        </v-layout>
+                    </v-flex>
+                    <pre>{{result}}</pre>
+                </v-layout>
             </v-flex>
         </v-layout>
     </v-container>
@@ -10,9 +69,26 @@
 
 <script>
     export default {
-        date() {
+        data() {
             return {
-
+                title: '',
+                description: '',
+                promo: false,
+                valid: false,
+                result: null,
+            }
+        },
+        methods: {
+            createAd() {
+                if (this.$refs.form.validate()) {
+                    //
+                    const ad = {
+                        title: this.title,
+                        description: this.description,
+                        promo: this.promo,
+                    }
+                    this.result = ad;
+                }
             }
         }
     }
--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,44 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Orders</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3">Orders</h1>
+                <v-list
+                        subheader
+                        two-line
+                        flat
+                >
+                    <v-list-item-group
+                            multiple
+                    >
+                        <v-list-item
+
+                            v-for="order in orders"
+                            :key="order.key"
+                        >
+                            <template v-slot:default="{ active, toggle }">
+                                <v-list-item-action>
+                                    <v-checkbox
+                                            :input-value="order.done"
+                                            color="success"
+                                            @click="markDone(order)"
+                                    ></v-checkbox>
+                                </v-list-item-action>
+
+                                <v-list-item-content>
+                                    <v-list-item-title>{{order.name}}</v-list-item-title>
+                                    <v-list-item-subtitle>{{order.phone}}</v-list-item-subtitle>
+                                </v-list-item-content>
+                                <v-list-item-action>
+                                    <v-btn
+                                            class="primary"
+                                            :to="'/add/' + order.adId"
+                                    >Open</v-btn>
+                                </v-list-item-action>
+                            </template>
+                        </v-list-item>
+                    </v-list-item-group>
+                </v-list>
             </v-flex>
         </v-layout>
     </v-container>
@@ -10,10 +46,25 @@
 
 <script>
     export default {
-        date() {
+        data() {
             return {
+                orders: [
+                    {
+                        id: 'dsf',
+                        name: 'Vlad',
+                        phone: '8-955-464-64-43',
+                        adId: '123',
+                        done: false,
+                    }
+                ]
 
             }
+        },
+        methods: {
+            markDone(order) {
+                order.done = true;
+            }
+
         }
     }
 </script>
