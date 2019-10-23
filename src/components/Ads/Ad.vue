01e1c9ec2301a2daff799fe91cd513e4b117ba48
AntonS <so4usertrue@gmail.com>
2019-10-24 02:19:24 +0300

    Create modal window edit ad

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,40 +1,50 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-              <v-card>
-                <v-img
-                  :src="ad.imageSrc"
-                  height="300px"
-                  >
-                </v-img>
-                <v-card-text>
-                  <h1 class="text--primary">{{ad.title}}</h1>
-                  <p>{{ad.description}}</p>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn class="warning" text>Edit</v-btn>
-                      <v-btn class="success">Buy</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        <v-card v-if="!loading">
+          <v-img :src="ad.imageSrc" height="300px"> </v-img>
+          <v-card-text>
+            <h1 class="text--primary">{{ ad.title }}</h1>
+            <p>{{ ad.description }}</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <app-edit-ad-modal :ad="ad"></app-edit-ad-modal>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
+        <div v-else class="text-center pt-5">
+          <v-progress-circular
+            :size="100"
+            :width="4"
+            color="purple"
+            indeterminate
+          ></v-progress-circular>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-    export default {
-      props: ['id'],
-      computed: {
-        ad() {
-          const id = this.id;
-          return this.$store.getters.adById(id);
-        }
-      }
+import EditAdModal from "./EditAdModal";
+
+export default {
+  props: ["id"],
+  computed: {
+    ad() {
+      const id = this.id;
+      return this.$store.getters.adById(id);
+    },
+    loading() {
+      return this.$store.getters.loading;
     }
+  },
+  components: {
+    appEditAdModal: EditAdModal
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,107 +1,92 @@
 <template>
-    <div v-if="!loading">
-        <v-container fluid>
-            <v-layout row>
-                <v-flex xs12>
-                    <v-carousel>
-                        <v-carousel-item
-                                v-for="ad of promoAds"
-                                :key="ad.key"
-                                :src="ad.imageSrc"
-                        >
-                            <div class="car-link">
-                                <v-btn class="error" :to="'/ad/' + ad.id">{{ad.title}}</v-btn>
-                            </div>
-                        </v-carousel-item>
-                    </v-carousel>
-                </v-flex>
-            </v-layout>
-        </v-container>
-        <v-container grid-list-lg   >
-            <v-layout row>
-                <v-flex
-                        xs12
-                        sm6
-                        md4
-                        v-for="ad of ads"
-                        v-bind:key="ad.id"
-                >
-                    <v-card
-                            class="mx-auto"
-                            max-width="400"
-                    >
-                        <v-img
-                                class="white--text align-end"
-                                height="200px"
-                                :src="ad.imageSrc"
-                        >
-                            <v-card-title>{{ad.title}}</v-card-title>
-                        </v-img>
+  <div v-if="!loading">
+    <v-container fluid>
+      <v-layout row>
+        <v-flex xs12>
+          <v-carousel>
+            <v-carousel-item
+              v-for="ad of promoAds"
+              :key="ad.key"
+              :src="ad.imageSrc"
+            >
+              <div class="car-link">
+                <v-btn class="error" :to="'/ad/' + ad.id">{{ ad.title }}</v-btn>
+              </div>
+            </v-carousel-item>
+          </v-carousel>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container grid-list-lg>
+      <v-layout row>
+        <v-flex xs12 sm6 md4 v-for="ad of ads" v-bind:key="ad.id">
+          <v-card class="mx-auto" max-width="400">
+            <v-img
+              class="white--text align-end"
+              height="200px"
+              :src="ad.imageSrc"
+            >
+              <v-card-title>{{ ad.title }}</v-card-title>
+            </v-img>
 
-                        <v-card-text class="text--primary">
-                            <div>{{ad.description}}</div>
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-btn
-                                    text
-                                    :to="'/ad/' + ad.id"
-                            >
-                                Share
-                            </v-btn>
+            <v-card-text class="text--primary">
+              <div>{{ ad.description }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text :to="'/ad/' + ad.id">
+                Share
+              </v-btn>
 
-                            <v-btn
-                                    raised
-                                    class="primary"
-                            >
-                                Buy
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-flex>
-            </v-layout>
-        </v-container>
-    </div>
-    <div v-else>
-      <v-container>
-        <v-layout row>
-          <v-flex xs12 class="text-center pt-5">
-                <v-progress-circular
-                  :size="100"
-                  :width="4"
-                  color="purple"
-                  indeterminate
-                ></v-progress-circular>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </div>
+              <v-btn raised class="primary">
+                Buy
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+  <div v-else>
+    <v-container>
+      <v-layout row>
+        <v-flex xs12 class="text-center pt-5">
+          <v-progress-circular
+            :size="100"
+            :width="4"
+            color="purple"
+            indeterminate
+          ></v-progress-circular>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
-    export default {
-      computed: {
-        promoAds() {
-          return this.$store.getters.promoAds;
-        },
-        ads() {
-          return this.$store.getters.ads;
-        },
-        loading() {
-          return this.$store.getters.loading;
-        }
-      }
+export default {
+  computed: {
+    promoAds() {
+      return this.$store.getters.promoAds;
+    },
+    ads() {
+      return this.$store.getters.ads;
+    },
+    loading() {
+      return this.$store.getters.loading;
     }
+  }
+};
 </script>
 
 <style scoped>
-    .car-link{
-        position: absolute;
-        bottom: 50px;
-        left: 50%;
-        background: rgba(0, 0, 0, .5);
-        transform: translate(-50%, 0);
-        padding: 5px 15px;
-        border-top-left-radius: 5px;
-        border-top-right-radius: 5px;
-    }
+.car-link {
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  background: rgba(0, 0, 0, 0.5);
+  transform: translate(-50%, 0);
+  padding: 5px 15px;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
 </style>
